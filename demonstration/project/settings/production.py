5e25f1a4a3d08d31df96a81a902d7906dd767f79
David THENON <sveetch@gmail.com>
2024-08-21 15:58:25 +0000

    [CHG] Added versioning marks in generic template and view

--- demonstration/project/settings/production.py
+++ demonstration/project/settings/production.py
@@ -5,8 +5,9 @@ from settings.base import *  # noqa: F403
 
 DEBUG = False
 
-# Directory where all stuff will be builded
-PUBLISH_DIR = os.path.join(REPOSITORY_DIR, "dist", "prod")  # noqa: F405
+# Directly build into the "docs/" directory for github page
+PUBLISH_DIR = os.path.join(REPOSITORY_DIR, "docs")  # noqa: F405
+
 # Path where will be moved all the static files, usually this is a directory in
 # the ``PUBLISH_DIR``
 STATIC_DIR = os.path.join(PUBLISH_DIR, "static")  # noqa: F405
--- demonstration/project/views/generic.py
+++ demonstration/project/views/generic.py
@@ -1,5 +1,8 @@
+import json
 from gettext import gettext as _
+from pathlib import Path
 
+from optimus import __version__
 from optimus.pages.views import PageTemplateView
 
 
@@ -10,3 +13,21 @@ class GenericPageView(PageTemplateView):
     title = _("Generic basic page")
     template_name = "generic.html"
     destination = "{language_code}/index.html"
+
+    def get_context(self):
+        """
+        Add Optimus and template versions
+        """
+        super().get_context()
+
+        template_version = None
+        template_manifest = Path(self.settings.REPOSITORY_DIR) / "cookiebaked.json"
+        if template_manifest.exists():
+            manifest = json.loads(template_manifest.read_text())
+            template_version = manifest.get("_optimus_bootstrap_version")
+
+        self.context.update({
+            "optimus_version": __version__,
+            "template_version": template_version,
+        })
+        return self.context
